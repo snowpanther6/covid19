33315b0d702b6df50269ba5fd06bd0c6d5641051
Shailesh Yogendra <53175687+snowpanther6@users.noreply.github.com>
2020-04-18 14:48:17 +0530

    done

---
+++ build/precache-manifest.b4943d18a6650b316d85a8e0932af328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ca7ac2db26928c980430c8bf32f960",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "73fef24864701e1c8672",
+    "url": "/covid19/static/css/main.85945c29.chunk.css"
+  },
+  {
+    "revision": "a8d5f599418ae1754db9",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fef24864701e1c8672",
+    "url": "/covid19/static/js/main.ec70f79d.chunk.js"
+  },
+  {
+    "revision": "384fdf37c74f6a26a4c0",
+    "url": "/covid19/static/js/runtime-main.2993b3e3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
